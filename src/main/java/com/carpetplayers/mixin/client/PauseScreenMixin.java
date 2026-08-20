86429cfd523a1befbd16f9f7856bc32bf6c20ade
HidayatBelajar319 <hidayat3911@sd.belajar.id>
2026-08-20 20:09:34 +0800

    Fix: remove @Shadow from TitleScreenMixin/PauseScreenMixin causing crash

--- src/main/java/com/carpetplayers/mixin/client/PauseScreenMixin.java
+++ src/main/java/com/carpetplayers/mixin/client/PauseScreenMixin.java
@@ -1,13 +1,11 @@
 package com.carpetplayers.mixin.client;
 
 import com.carpetplayers.client.gui.CarpetPlayersScreen;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,9 +15,6 @@ public abstract class PauseScreenMixin extends Screen {
     protected PauseScreenMixin(TextComponent title) {
         super(title);
     }
-
-    @Shadow
-    protected abstract <T extends AbstractWidget> T addButton(T button);
 
     @Inject(method = "init", at = @At("RETURN"))
     private void carpetplayers_addPauseMenuButton(CallbackInfo ci) {
--- src/main/java/com/carpetplayers/mixin/client/TitleScreenMixin.java
+++ src/main/java/com/carpetplayers/mixin/client/TitleScreenMixin.java
@@ -1,13 +1,11 @@
 package com.carpetplayers.mixin.client;
 
 import com.carpetplayers.client.gui.CarpetPlayersScreen;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,9 +15,6 @@ public abstract class TitleScreenMixin extends Screen {
     protected TitleScreenMixin(TextComponent title) {
         super(title);
     }
-
-    @Shadow
-    protected abstract <T extends AbstractWidget> T addButton(T button);
 
     @Inject(method = "init", at = @At("RETURN"))
     private void carpetplayers_addMenuButton(CallbackInfo ci) {
