298b5877cdc50a59db68fea52e2d6064c84835d8
HidayatBelajar319 <hidayat3911@sd.belajar.id>
2026-08-20 21:03:22 +0800

    Fix: reposition PauseScreen button above Back to Game

--- src/main/java/com/carpetplayers/mixin/client/PauseScreenMixin.java
+++ src/main/java/com/carpetplayers/mixin/client/PauseScreenMixin.java
@@ -18,8 +18,9 @@ public abstract class PauseScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("RETURN"))
     private void carpetplayers_addPauseMenuButton(CallbackInfo ci) {
+        // Place button above "Back to Game" in the pause menu
         this.addButton(
-            new Button(4, 4, 120, 20, new TextComponent("Carpet Players"),
+            new Button(this.width / 2 - 100, this.height / 4 - 34, 200, 20, new TextComponent("Carpet Players"),
                 button -> net.minecraft.client.Minecraft.getInstance().setScreen(
                     new CarpetPlayersScreen(this)
                 )
